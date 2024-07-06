9bafef5510521fe050bf346c00b6948d3f19b157
Tmk <i@tmk.im>
2024-07-06 17:50:33 +0800

    feat: add release ci workflow

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig(({ watch }) => ({
   entry: ['./src/*.ts', '!./src/**/*.{test,spec}.ts'],
   outDir:
-    watch || process.env.CI
+    watch || process.env.CI === 'true'
       ? // it must be under `node_modules`
         './node_modules/storybook-playwright-ct/dist'
       : './dist',
